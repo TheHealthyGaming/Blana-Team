e6945acf34441b0da85c437c2d5724e3ba61c37c
TheHealthyGaming <94680385+TheHealthyGaming@users.noreply.github.com>
2021-12-05 16:57:51 +0200

    Add files via upload

---
+++ Green/assets/js/main.js
@@ -0,0 +1,229 @@
+(function () {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
+
+  /**
+   * Easy on scroll event listener
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener("scroll", listener);
+  };
+
+  /**
+   * Header fixed top on scroll
+   */
+  let selectHeader = select("#header");
+  if (selectHeader) {
+    let headerOffset = selectHeader.offsetTop;
+    let nextElement = selectHeader.nextElementSibling;
+    const headerFixed = () => {
+      if (headerOffset - window.scrollY <= 0) {
+        selectHeader.classList.add("fixed-top");
+        nextElement.classList.add("scrolled-offset");
+      } else {
+        selectHeader.classList.remove("fixed-top");
+        nextElement.classList.remove("scrolled-offset");
+      }
+    };
+    window.addEventListener("load", headerFixed);
+    onscroll(document, headerFixed);
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select(".back-to-top");
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add("active");
+      } else {
+        backtotop.classList.remove("active");
+      }
+    };
+    window.addEventListener("load", toggleBacktotop);
+    onscroll(document, toggleBacktotop);
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on("click", ".mobile-nav-toggle", function (e) {
+    select("#navbar").classList.toggle("navbar-mobile");
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
+  });
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on(
+    "click",
+    ".navbar .dropdown > a",
+    function (e) {
+      if (select("#navbar").classList.contains("navbar-mobile")) {
+        e.preventDefault();
+        this.nextElementSibling.classList.toggle("dropdown-active");
+      }
+    },
+    true
+  );
+
+  //  Clients Slider
+
+  new Swiper(".clients-slider", {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: "auto",
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 60,
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 80,
+      },
+      992: {
+        slidesPerView: 6,
+        spaceBetween: 120,
+      },
+    },
+  });
+
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener("load", () => {
+    let portfolioContainer = select(".portfolio-container");
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: ".portfolio-item",
+      });
+
+      let portfolioFilters = select("#portfolio-flters li", true);
+
+      on(
+        "click",
+        "#portfolio-flters li",
+        function (e) {
+          e.preventDefault();
+          portfolioFilters.forEach(function (el) {
+            el.classList.remove("filter-active");
+          });
+          this.classList.add("filter-active");
+
+          portfolioIsotope.arrange({
+            filter: this.getAttribute("data-filter"),
+          });
+        },
+        true
+      );
+    }
+  });
+
+  /**
+   * Initiate portfolio lightbox
+   */
+  const portfolioLightbox = GLightbox({
+    selector: ".portfolio-lightbox",
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper(".portfolio-details-slider", {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+  });
+})();
+
+document.querySelector("#submit").onclick = () => {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const title = document.getElementById("title").value;
+  const message = document.getElementById("message").value;
+  
+  const rate1 = document.getElementById("rate-1").checked;
+  const rate2 = document.getElementById("rate-2").checked;
+  const rate3 = document.getElementById("rate-3").checked;
+  const rate4 = document.getElementById("rate-4").checked;
+  const rate5 = document.getElementById("rate-5").checked;
+
+  let stars = 0;
+  if(rate1) stars = 1;
+  if(rate2) stars = 2;
+  if(rate3) stars = 3;
+  if(rate4) stars = 4;
+  if(rate5) stars = 5;
+
+  const rating = { name, email, title, message, stars };
+  const reviews = JSON.parse(localStorage.getItem("reviews") || "[]");
+  reviews.push(rating);
+  localStorage.setItem("reviews", JSON.stringify(reviews));
+  showReviews();
+};
+
+function showReviews() {
+  const list = document.querySelector("#reviews");
+  const reviews = JSON.parse(localStorage.getItem("reviews") || "[]");
+
+  let str = "";
+  reviews.forEach((review) => {
+    str += `<h4>${review.name} - ${review.title}</h4>
+    <div class="item star-rating">
+    ${"<label class=\"fas fa-star\"></label>".repeat(review.stars)}
+    </div> <br></br>
+            <p class="item message">${review.message}</p>`;
+  });
+  list.innerHTML = str;
+}
+
+showReviews();
